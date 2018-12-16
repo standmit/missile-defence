6273fbfb0aed5f8ae5f45c08976633c8b1563271
standmit <standmit@yandex.ru>
2018-12-16 16:15:10 +0700

    temporary disable difficalt increasing

--- missile_defence.py
+++ missile_defence.py
@@ -160,7 +160,7 @@ class MissileDefenceGame(object):
         self.fire_cycle = 0
         self.shield_dome = ShieldDome(self.resolution)
         self.shield_dome.health = 2
-        self.missile_threshold = 0.02
+        self.missile_threshold = 0.01
         self.projectiles = []
         self.initial_buildings_sum = self.get_buildings_sum()
         self.buildings_sum = self.initial_buildings_sum
@@ -332,7 +332,7 @@ class MissileDefenceGame(object):
                 self.score += 100
                            
             # randomly add more projectiles
-            self.missile_threshold += 0.00001
+            #self.missile_threshold += 0.000001
             
             m = self.missile_threshold
             while m > 0:
